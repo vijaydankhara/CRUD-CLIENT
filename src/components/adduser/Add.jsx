cd675699343134ab46dd39593a3c9bc749c5cdc8
vijay dankhara <vijaydankhara111@gmail.com>
2024-11-12 11:53:26 +0530

    12-11-2024

--- src/components/adduser/Add.jsx
+++ src/components/adduser/Add.jsx
@@ -26,7 +26,8 @@ const Add = () => {
   const submitForm = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8000/api/create", user);
+      // const response = await axios.post("http://localhost:8000/api/create", user);
+      const response = await axios.post("https://crud-server-70av.onrender.com/api/create", user);
       toast.success(response.data.msg, { position: 'top-right' });
       navigate("/");
     } catch (error) {
